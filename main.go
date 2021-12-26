c7b66446a00f876c2f7349eaeb05a58173511aad
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-12-25 21:27:59 -0500

    Adding department route.

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"net/http"
 
+	"adventureVsModule/config"
+	"adventureVsModule/pkg/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	config.ConnectDatabase()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
+	r.GET("/departments", controller.List)
+
 	r.Run()
 }
