e4fc6ec7429b0361b13862c8717847fd9623aab1
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-12-25 20:55:33 -0500

    Creating the database connection.

---
+++ config/setup.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Not exist enviroment.")
+	}
+
+	server := os.Getenv("SERVER")
+	port := os.Getenv("PORT")
+	database := os.Getenv("DATABASE")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s", user, password, server, port, database)
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Wrong connection.")
+	}
+
+	DB = db
+}
