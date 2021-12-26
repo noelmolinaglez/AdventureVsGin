dd6d930efef63b0978b2007b277ec9119eb10207
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-12-26 16:55:51 -0500

    Logging departmentsController.

--- pkg/controller/departmentController.go
+++ pkg/controller/departmentController.go
@@ -2,11 +2,20 @@ package controller
 
 import (
 	"adventureVsModule/pkg/Repository/humanResources"
+	constants "adventureVsModule/pkg/utils"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	departmentController = "DepartmentController"
+	lDepartments         = "ListDepartments"
 )
 
 func ListDepartments(c *gin.Context) {
+	log.WithFields(log.Fields{constants.FileName: departmentController, constants.FunctionName: lDepartments}).Info(constants.StartFunction)
 
+	log.WithFields(log.Fields{constants.FileName: departmentController, constants.FunctionName: lDepartments}).Info(constants.EndFunction)
 	humanResources.ListDepartments(c)
 
 }
