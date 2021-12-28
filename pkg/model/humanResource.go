f7bbde1fa5f84e7b48e9e612b83cb7987efe1cbe
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-12-27 21:31:13 -0500

    Adding the Employee dto.

--- pkg/model/humanResource.go
+++ pkg/model/humanResource.go
@@ -6,3 +6,22 @@ type Department struct {
 	GroupName    string `json:"GroupName"`
 	ModifiedDate string `json:"ModifiedDate"`
 }
+
+type Employee struct {
+	BusinessEntityId  uint   `json:"BusinessEntityID" gorm:"primary_key"`
+	NationalIdNumber  string `json:"NationalIDNumber"`
+	LoginId           string `json:"loginID"`
+	OrganizationNode  string `json:"OrganizationNode"`
+	OrganizationLevel string `json:"OrganizationLevel"`
+	JobTitle          string `json:"JobTitle"`
+	BirthDate         string `json:"BirthDate"`
+	MaritalStatus     string `json:"MaritalStatus"`
+	Gender            string `json:"Gender"`
+	HiredDate         string `json:"HiredDate"`
+	SalariedFlag      bool   `json:"SalariedFlag"`
+	VacationHours     uint   `json:"VacationHours"`
+	SickLeaveHours    uint   `json:"SickLeaveHours"`
+	CurrentFlag       bool   `json:"CurrentFlag"`
+	RowGuid           string `json:"rowguid"`
+	ModifiedDate      string `json:"ModifiedDate"`
+}
