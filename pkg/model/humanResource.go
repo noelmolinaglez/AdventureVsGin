d0494683c5739b1f9b75233c3f27639749bb6114
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-12-28 21:22:56 -0500

    Fixing bug parsing id.

--- pkg/model/humanResource.go
+++ pkg/model/humanResource.go
@@ -1,7 +1,7 @@
 package model
 
 type Department struct {
-	DepartmentId uint   `json:"DepartmentID" gorm:"primary_key"`
+	DepartmentID uint   `json:"DepartmentID"`
 	Name         string `json:"Name"`
 	GroupName    string `json:"GroupName"`
 	ModifiedDate string `json:"ModifiedDate"`
