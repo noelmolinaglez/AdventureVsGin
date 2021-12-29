a21ee142ce31c0b48d13216fd12993c672bbd583
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-12-28 21:38:13 -0500

    Adding field and dir fields to request.

--- pkg/dto/request.go
+++ pkg/dto/request.go
@@ -1,6 +1,8 @@
 package dto
 
 type Request struct {
-	Start int `form:"start"`
-	Limit int `form:"limit"`
+	Start int    `form:"start"`
+	Limit int    `form:"limit"`
+	Field string `form:"field"`
+	Dir   string `form:"dir"`
 }
