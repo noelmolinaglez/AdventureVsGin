782a9929bfb61ea2652204cf558ce16b139090d7
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-01-03 21:10:53 -0500

    Mapping the model data as part of the request.

--- pkg/dto/request.go
+++ pkg/dto/request.go
@@ -1,10 +1,11 @@
 package dto
 
 type Request struct {
-	Action string `form:"action"`
-	Type   string `form:"type"`
-	Start  int    `form:"start"`
-	Limit  int    `form:"limit"`
-	Field  string `form:"field"`
-	Dir    string `form:"dir"`
+	Action string      `form:"action"`
+	Type   string      `form:"type"`
+	Start  int         `form:"start"`
+	Limit  int         `form:"limit"`
+	Field  string      `form:"field"`
+	Dir    string      `form:"dir"`
+	Data   interface{} `form:"data"`
 }
