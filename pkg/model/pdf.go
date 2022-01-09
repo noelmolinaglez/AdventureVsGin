e2c434028a15c82d57e809f03013b080e01d7ef2
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-01-09 13:24:01 -0500

    Declaring departments custom headers.

--- pkg/model/pdf.go
+++ pkg/model/pdf.go
@@ -2,20 +2,55 @@ package model
 
 import (
 	"github.com/noelmolinaglez/simplePdf/pkg/dto"
-	"github.com/noelmolinaglez/simplePdf/pkg/helpers"
-	"strconv"
 )
 
 func (Department) GetHeaders() []dto.CellStruct {
-	columns := []string{
-		"DepartmentId",
-		"Name",
-		"GroupName",
-		"ModifiedDate",
+
+	headers := make([]dto.CellStruct, 4)
+
+	headers[0] = dto.CellStruct{
+		Width:   2,
+		Height:  10,
+		Content: "DepartmentId",
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
 	}
-	headers := make([]dto.CellStruct, len(columns))
-	for i, _ := range headers {
-		headers[i] = helpers.CreateSimpleCell(columns[i])
+	headers[1] = dto.CellStruct{
+		Width:   2,
+		Height:  10,
+		Content: "Name",
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
+	}
+	headers[2] = dto.CellStruct{
+		Width:   3,
+		Height:  10,
+		Content: "GroupName",
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
+	}
+	headers[3] = dto.CellStruct{
+		Width:   5,
+		Height:  10,
+		Content: "ModifiedDate",
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
 	}
 
 	return headers
@@ -24,10 +59,10 @@ func (Department) GetHeaders() []dto.CellStruct {
 func (d Department) GetValues() []dto.CellStruct {
 
 	values := make([]dto.CellStruct, 4)
-	values[0] = helpers.CreateSimpleCell(strconv.Itoa(int(d.DepartmentID)))
-	values[1] = helpers.CreateSimpleCell(d.Name)
-	values[2] = helpers.CreateSimpleCell(d.GroupName)
-	values[3] = helpers.CreateSimpleCell(d.ModifiedDate)
+	//values[0] = helpers.CreateSimpleCell(strconv.Itoa(int(d.DepartmentID)))
+	//values[1] = helpers.CreateSimpleCell(d.Name)
+	//values[2] = helpers.CreateSimpleCell(d.GroupName)
+	//values[3] = helpers.CreateSimpleCell(d.ModifiedDate)
 
 	return values
 }
