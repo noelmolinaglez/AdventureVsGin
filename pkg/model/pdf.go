2de8c0ebcedaca48e8d92a13d54dcc88f1a11977
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-01-09 13:45:06 -0500

    Finishing the departments headers and values.

--- pkg/model/pdf.go
+++ pkg/model/pdf.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"github.com/noelmolinaglez/simplePdf/pkg/dto"
+	"strconv"
 )
 
 func (Department) GetHeaders() []dto.CellStruct {
@@ -20,7 +21,7 @@ func (Department) GetHeaders() []dto.CellStruct {
 		LinkStr: "",
 	}
 	headers[1] = dto.CellStruct{
-		Width:   2,
+		Width:   3,
 		Height:  10,
 		Content: "Name",
 		Border:  "1",
@@ -31,7 +32,7 @@ func (Department) GetHeaders() []dto.CellStruct {
 		LinkStr: "",
 	}
 	headers[2] = dto.CellStruct{
-		Width:   3,
+		Width:   4,
 		Height:  10,
 		Content: "GroupName",
 		Border:  "1",
@@ -42,7 +43,7 @@ func (Department) GetHeaders() []dto.CellStruct {
 		LinkStr: "",
 	}
 	headers[3] = dto.CellStruct{
-		Width:   5,
+		Width:   3,
 		Height:  10,
 		Content: "ModifiedDate",
 		Border:  "1",
@@ -59,10 +60,53 @@ func (Department) GetHeaders() []dto.CellStruct {
 func (d Department) GetValues() []dto.CellStruct {
 
 	values := make([]dto.CellStruct, 4)
-	//values[0] = helpers.CreateSimpleCell(strconv.Itoa(int(d.DepartmentID)))
-	//values[1] = helpers.CreateSimpleCell(d.Name)
-	//values[2] = helpers.CreateSimpleCell(d.GroupName)
-	//values[3] = helpers.CreateSimpleCell(d.ModifiedDate)
+	values[0] = dto.CellStruct{
+		Width:   2,
+		Height:  10,
+		Content: strconv.Itoa(int(d.DepartmentID)),
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
+	}
+
+	values[1] = dto.CellStruct{
+		Width:   3,
+		Height:  10,
+		Content: d.Name,
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
+	}
+
+	values[2] = dto.CellStruct{
+		Width:   4,
+		Height:  10,
+		Content: d.GroupName,
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
+	}
+
+	values[3] = dto.CellStruct{
+		Width:   3,
+		Height:  10,
+		Content: d.ModifiedDate,
+		Border:  "1",
+		Ln:      0,
+		Align:   "C",
+		Fill:    false,
+		Link:    0,
+		LinkStr: "",
+	}
 
 	return values
 }
